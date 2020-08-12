ca11fdc8fdc95b8411b47fa8b06e7e0aa6908db0
Petr <mail@efimov-it.info>
2020-08-12 17:52:34 +0300

    Fix auth and add script for check application status

---
+++ api/v1/applications/status/index.php
@@ -0,0 +1,118 @@
+<?php
+
+if (isset($_GET['key']) && !empty($_GET['key']))
+    $key = trim(htmlspecialchars(stripslashes($_GET['key'])));
+else exit(json_encode(array(
+         'status' => 'error',
+         'message' => 'key is empty'
+     )));
+
+require_once '../../db_connect.php';
+
+$query = "select *
+          from applications
+          where id_application = $key";
+
+$result = mysqli_query($connection, $query) or
+          exit(json_encode(array(
+              'status' => 'error',
+              'message' => 'Data base error',
+              'mysql_error' => mysqli_error($connection)
+          )));
+
+if (mysqli_num_rows($result) > 0) {
+    $application_row = mysqli_fetch_array($result);
+    $application = array();
+    $application['id_application'] = $application_row['id_application'];
+    $application['date'] = $application_row['date'];
+    $application['application_text'] = $application_row['application_text'];
+    $application['status'] = "created";
+
+    $query = "select *
+              from processing
+              where id_application = " . $application['id_application'];
+
+    $result = mysqli_query($connection, $query) or
+              exit(json_encode(array(
+                  'status' => 'error',
+                  'message' => 'Data base error',
+                  'mysql_error' => mysqli_error($connection)
+              )));
+
+    if(mysqli_num_rows($result) > 0) {
+        $processing = mysqli_fetch_array($result);
+
+        if ($processing['id_user'] == $processing['id_operator']) {
+            $query = "select *
+                      from users
+                      where id_user = " . $processing['id_user'];
+
+            $result = mysqli_query($connection, $query) or
+                      exit(json_encode(array(
+                          'status' => 'error',
+                          'message' => 'Data base error',
+                          'mysql_error' => mysqli_error($connection)
+                      )));
+            
+            if (mysqli_num_rows($result) > 0) {
+                $user = mysqli_fetch_array($result);
+                $application['support_fullname'] = $user['full_name'];
+                $application['support_post'] = $user['user_post'];
+            }
+        }
+        else {
+            $query = "select *
+                      from users
+                      where id_user = in(" . $processing['id_user'] . ", " . $processing['id_operator'] . ")";
+
+            $result = mysqli_query($connection, $query) or
+                      exit(json_encode(array(
+                          'status' => 'error',
+                          'message' => 'Data base error',
+                          'mysql_error' => mysqli_error($connection)
+                      )));
+            
+            if (mysqli_num_rows($result) > 1) {
+                $user = mysqli_fetch_array($result);
+                $operator = mysqli_fetch_array($result);
+                $application['support_fullname'] = $user['full_name'];
+                $application['support_post'] = $user['user_post'];
+                $application['operator_fullname'] = $operator['full_name'];
+                $application['operator_post'] = $operator['user_post'];
+            }
+        }
+
+        $application['processing_date'] = $processing['date'];
+        $application['status'] = "processing";
+
+        $query = "select id_completed
+                  from completed
+                  where id_processing = " . $processing['id_processing'];
+
+        $result = mysqli_query($connection, $query) or
+                  exit(json_encode(array(
+                    'status' => 'error',
+                    'message' => 'Data base error',
+                    'mysql_error' => mysqli_error($connection)
+                  )));
+
+        if (mysqli_num_rows($result) > 0) {
+            $completed = mysqli_fetch_array($result);
+
+            $application['completed_date'] = $completed['date'];
+            $application['result_text'] = $completed['result_text'];
+            $application['status'] = "completed";
+        }
+    }
+
+    exit(json_encode(array(
+        'status' => 'success',
+        'data' => $application
+    )));
+}
+else {
+    exit(json_encode(array(
+        'status' => 'error',
+        'message' => 'Application not exist'
+    )));
+}
--- api/v1/login/index.php
+++ api/v1/login/index.php
@@ -21,9 +21,9 @@ else {
 
 require_once('../db_connect.php');
 
-$query = "select *
+$query = 'select *
           from users
-          where login like \"$login\" and password like \"$password\"";
+          where login like "'.$login.'" and password like "'.$password.'"';
 
 $result = mysqli_query($connection, $query) or
           exit(json_encode(array(
@@ -32,7 +32,7 @@ $result = mysqli_query($connection, $query) or
               'mysql_error' => mysqli_error($connection)
           )));
 
-if ($result) {
+if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_array($result);
     $id_user = $row['id_user'];
 
@@ -40,53 +40,63 @@ if ($result) {
               from auth
               where id_user = $id_user";
     
-    $result = mysqli_query($connection, $query);
+    $result = mysqli_query($connection, $query) or
+              exit(json_encode(array(
+                  'status' => 'error',
+                  'messages' => 'Data base error',
+                  'mysql_error' => mysqli_error($connection)
+              )));
 
-    if ($result) {
-        if (mysqli_num_rows($result) == 1) {
-            $time = time();
-            $new_token = md5($time . $id_user);
-            $expires_in = $time + 86400;
-            $query = "update auth
-                      set date = $time,
-                          token = \"$new_token\",
-                          expires_in = $expires_in
-                      where id_user = $id_user";
+    if (mysqli_num_rows($result) == 1) {
+        $time = time();
+        $new_token = md5($time . $id_user);
+        $expires_in = $time + 86400;
+        $query = "update auth
+                    set date = $time,
+                        token = \"$new_token\",
+                        expires_in = $expires_in
+                    where id_user = $id_user";
 
-            $result = mysqli_query($connection, $query);
+        $result = mysqli_query($connection, $query);
 
-            if ($result) {
-                exit (json_encode(array(
-                    'status' => 'success',
-                    'data' => array (
-                        'token' => $new_token,
-                        'expires_in' => $expires_in
-                    )
-                )));
-            }
+        if ($result) {
+            exit (json_encode(array(
+                'status' => 'success',
+                'data' => array (
+                    'token' => $new_token,
+                    'expires_in' => $expires_in
+                )
+            )));
+        }
+        echo 123;
+    }
+    else {
+        $time = time();
+        $token = md5($time . $id_user);
+        $expires_in = $time + 86400;
+        $query = "insert into auth
+                (token, date, id_user, expires_in)
+                values ('$token', $time, $id_user, $expires_in)";
+
+        $result = mysqli_query($connection, $query);
+
+        if ($result) {
+            exit (json_encode(array(
+                'status' => 'success',
+                'data' => array (
+                    'token' => $token,
+                    'expires_in' => $expires_in
+                )
+            )));
         }
         else {
-            $time = time();
-            $token = md5($time . $id_user);
-            $expires_in = $time + 86400;
-            $query = "insert into auth
-                    (token, date, id_user, expires_in)
-                    values ('$token', $time, $id_user, $expires_in)";
-
-            $result = mysqli_query($connection, $query);
-
-            if ($result) {
-                exit (json_encode(array(
-                    'status' => 'success',
-                    'data' => array (
-                        'token' => $token,
-                        'expires_in' => $expires_in
-                    )
-                )));
-            }
-            else {
-                echo mysqli_error($connection);
-            }
+            echo mysqli_error($connection);
         }
     }
+}
+else {
+    exit(json_encode(array(
+        'status' => 'error',
+        'message' => 'Invalid login or password'
+    )));
 }
